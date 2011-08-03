8f7e0306c4a56dc1366f73df09bf1381225ae978
David Robbins <robbins.david@gmail.com>
2011-08-02 20:58:54 -0500

    Fleshed out photo tutorial from CouchApp website (with minor adjustments for the current CouchApp version)

--- evently/items/_changes/data.js
+++ evently/items/_changes/data.js
@@ -1,10 +1,25 @@
 function(data) {
   // $.log(data)
-  var p;
+  var p
+		,	db = $$(this).app.db
+		;
+	function attachments(doc){
+		var atts = [];
+		if(doc._attachments){
+			for (var name in doc._attachments){
+				atts.push({
+					name : name,
+					path : ["",db.name,doc._id,name].join('/')
+				});
+			}
+		}
+		return atts;
+	}
   return {
     items : data.rows.map(function(r) {
       p = (r.value && r.value.profile) || {};
       p.message = r.value && r.value.message;
+			p.atts = attachments(r.value);
       return p;
     })
   }
